59c0f53796393d4c005b1ccc9e9f95fd9a1998fa
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2023-02-22 17:55:39 +0200

    add draft logic for base actions

--- libs/ui/DemoForm/DemoForm.tsx
+++ libs/ui/DemoForm/DemoForm.tsx
@@ -1,15 +1,15 @@
 
 import type { FC, Dispatch, SetStateAction } from 'react';
-import { MouseEvent, useRef } from 'react';
+import { MouseEvent, useRef, useCallback, useMemo } from 'react';
 import { ethers } from 'ethers'
 
 import Button from '@/libs/ui/Button';
 import { useWallet } from '@/feature/wallet'
-import { useNetworkProvider } from '@/libs/network'
+import { useNetworkProvider, GetProvider } from '@/libs/network'
 import { toBigNumERC20, fromBigNumERC20 } from '@/libs/decimals';
 
 import deploymentLock from '@dgma/protocol/deployment-lock.json'
-import abi from '@dgma/protocol/abi/contracts/app/facets/minter.sol/MinterFacet.json'
+import minterFacetAbi from '@dgma/protocol/abi/contracts/app/facets/minter.sol/MinterFacet.json'
 import styles from './DemoForm.module.css';
 
 const appDiamondAddress = deploymentLock.rabbit.AppDiamond.address
@@ -20,40 +20,56 @@ interface DemoFormProps {
   isTransactionPending: boolean
 }
 
+const getSigner = async (getProvider: GetProvider) => (await getProvider()).getSigner()
+
 const DemoForm: FC<DemoFormProps> = ({setTransactionPending, isTransactionPending}) => {
 
-  const { getProvider } = useNetworkProvider()
-  const { currentAccount } = useWallet()
+  const { getProvider } = useNetworkProvider();
+  const { currentAccount } = useWallet();
 
   const depositInput = useRef<HTMLInputElement>(null);
   const withdrawInput = useRef<HTMLInputElement>(null);
   const mintInput = useRef<HTMLInputElement>(null);
   const burnInput = useRef<HTMLInputElement>(null);
 
+  const handleLoading = useCallback(
+    async (ts: any) => {
+      setTransactionPending(true);
+      await ts.wait(1);
+      setTransactionPending(false);
+    },
+    [setTransactionPending]
+  )
+
   const deposit = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const val = depositInput?.current?.value;
+    // const signer = await getSigner(getProvider);
+    // const contract = new ethers.Contract(appDiamondAddress, vaultFacetAbi, signer);
+    // handleLoading(await contract.deposit(toBigNumERC20(val)))
     console.log('deposit input', val);
     console.log('deposit in bigint', toBigNumERC20(val));
     console.log('deposit in bigint.toString', toBigNumERC20(val).toString());
   }
   const withdraw = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    // console.log(withdrawInput?.current?.value);
+    // const val = depositInput?.current?.value;
+    // const signer = await getSigner(getProvider);
+    // const contract = new ethers.Contract(appDiamondAddress, vaultFacetAbi, signer);
+    // handleLoading(await contract.withdraw(toBigNumERC20(val)))
   }
   const mint = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    const signer = (await getProvider()).getSigner();
-    const contract = new ethers.Contract(appDiamondAddress, abi, signer)
-    const ctx = await contract.mint(tokenAddress, currentAccount);
-    setTransactionPending(true);
-    await ctx.wait(1);
-    setTransactionPending(false);
-    // console.log(mintInput?.current?.value);
+    const signer = await getSigner(getProvider);
+    const contract = new ethers.Contract(appDiamondAddress, minterFacetAbi, signer)
+    handleLoading(await contract.mint(tokenAddress, currentAccount))
   }
   const burn = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    // console.log(burnInput?.current?.value);
+    // const val = depositInput?.current?.value;
+    // const signer = await getSigner(getProvider);
+    // const contract = new ethers.Contract(appDiamondAddress, vaultFacetAbi, signer);
+    // handleLoading(await contract.burn(tokenAddress, toBigNumERC20(val)))
   }
 
   return (
