34ba5164a5a83e6e3806a39e2fda4124f0dc0fd8
Dzmitry Lahunouski <dzmitry_lahunouski@epam.com>
2023-02-22 15:42:34 +0200

    finish implementaion & ready for integration

--- libs/ui/DemoForm/DemoForm.tsx
+++ libs/ui/DemoForm/DemoForm.tsx
@@ -1,29 +1,123 @@
 
+import type { FC, Dispatch, SetStateAction } from 'react';
+import { MouseEvent, useRef } from 'react';
+import { ethers } from 'ethers'
+
 import Button from '@/libs/ui/Button';
+import { useWallet } from '@/feature/wallet'
+import { useNetworkProvider } from '@/libs/network'
+
+import deploymentLock from '@dgma/protocol/deployment-lock.json'
+import abi from '@dgma/protocol/abi/contracts/app/facets/minter.sol/MinterFacet.json'
 import styles from './DemoForm.module.css';
 
-const DemoForm = () => {
+const appDiamondAddress = deploymentLock.rabbit.AppDiamond.address
+const tokenAddress = deploymentLock.rabbit.USDgmTokenDiamond.address
+
+interface DemoFormProps {
+  setTransactionPending: Dispatch<SetStateAction<boolean>>
+  isTransactionPending: boolean
+}
+
+const DemoForm: FC<DemoFormProps> = ({setTransactionPending, isTransactionPending}) => {
+
+  const { getProvider } = useNetworkProvider()
+  const { currentAccount } = useWallet()
+
+  const depositInput = useRef<HTMLInputElement>(null);
+  const withdrawInput = useRef<HTMLInputElement>(null);
+  const mintInput = useRef<HTMLInputElement>(null);
+  const burnInput = useRef<HTMLInputElement>(null);
+
+  const deposit = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    // console.log(depositInput?.current?.value);
+  }
+  const withdraw = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    // console.log(withdrawInput?.current?.value);
+  }
+  const mint = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const signer = (await getProvider()).getSigner();
+    const contract = new ethers.Contract(appDiamondAddress, abi, signer)
+    const ctx = await contract.mint(tokenAddress, currentAccount);
+    setTransactionPending(true);
+    await ctx.wait(1);
+    setTransactionPending(false);
+    // console.log(mintInput?.current?.value);
+  }
+  const burn = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    // console.log(burnInput?.current?.value);
+  }
+
   return (
     <div className={styles.root}>
       <h3 className={styles.title}>PIGMY/USDgm Vault</h3>
       <div className={styles.row}>
           <div className={styles.group}>
-            <input type="number" placeholder="amount to deposit, PIGMY" className={styles.input}/>
-            <Button className={styles.btn}>Deposit</Button>
+            <input 
+              type="number" 
+              placeholder="amount to deposit, PIGMY" 
+              className={styles.input} 
+              ref={depositInput}
+              disabled={isTransactionPending}
+            />
+            <Button 
+              className={styles.btn} 
+              onClick={deposit}
+              disabled={isTransactionPending}
+            >
+                Deposit
+            </Button>
           </div>
           <div className={styles.group}>
-            <input type="number" placeholder="amount to withdraw, PIGMY" className={styles.input}/>
-            <Button className={styles.btn}>Withdraw</Button>
+            <input 
+              type="number" 
+              placeholder="amount to withdraw, PIGMY" 
+              className={styles.input} 
+              ref={withdrawInput}
+              disabled={isTransactionPending}/>
+            <Button 
+              className={styles.btn} 
+              onClick={withdraw}
+              disabled={isTransactionPending}
+            >
+              Withdraw
+            </Button>
           </div>
         </div>
         <div className={styles.row}>
           <div className={styles.group}>
-            <input type="number" placeholder="amount to mint, USDgm" className={styles.input}/>
-            <Button className={styles.btn}>Mint</Button>
+            <input 
+              type="number" 
+              placeholder="amount to mint, USDgm" 
+              className={styles.input} 
+              ref={mintInput}
+              disabled={isTransactionPending}/>
+            <Button 
+              className={styles.btn} 
+              onClick={mint}
+              disabled={isTransactionPending}
+            >
+              Mint
+            </Button>
           </div>
           <div className={styles.group}>
-            <input type="number" placeholder="amount to burn, USDgm" className={styles.input}/>
-            <Button className={styles.btn}>Burn</Button>
+            <input 
+              type="number" 
+              placeholder="amount to burn, USDgm" 
+              className={styles.input} 
+              ref={burnInput}
+              disabled={isTransactionPending}/>
+            <Button 
+              className={styles.btn} 
+              onClick={burn}
+              disabled={isTransactionPending}
+            >
+              Burn
+            </Button>
           </div>
         </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,14 @@
-import { ethers } from 'ethers'
+import { useState } from 'react'
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
-
-import Button from '@/libs/ui/Button'
-
 import styles from '@/styles/Home.module.css'
-import { WalletProvider, ConnectToMetaMask } from '@/feature/wallet'
-import { useNetworkProvider } from '@/libs/network'
-import { useWallet } from '@/feature/wallet'
+import { WalletProvider, Account } from '@/feature/wallet'
 import { DemoForm } from '@/libs/ui/DemoForm';
 
-import deploymentLock from '@dgma/protocol/deployment-lock.json'
-import abi from '@dgma/protocol/abi/contracts/app/facets/minter.sol/MinterFacet.json'
-
-const appDiamondAddress = deploymentLock.rabbit.AppDiamond.address
-const tokenAddress = deploymentLock.rabbit.USDgmTokenDiamond.address
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-  const { getProvider } = useNetworkProvider()
-  const { currentAccount } = useWallet()
 
-  const onMint = async () => {
-    const provider = await getProvider()
-    const contract = new ethers.Contract(appDiamondAddress, abi, provider.getSigner())
-    await contract.mint(tokenAddress, currentAccount)
-  }
-
+  const [isTransactionPending, setTransactionPending] = useState(false);
+  
   return (
     <WalletProvider>
       <Head>
@@ -39,10 +20,9 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <h1>Dogma</h1>
-          <ConnectToMetaMask />
+          <Account pending={isTransactionPending}/>
         </div>
-        <DemoForm />
-        <Button onClick={onMint}>Mint</Button>
+        <DemoForm setTransactionPending={setTransactionPending} isTransactionPending={isTransactionPending} />
       </main>
     </WalletProvider>
   )
