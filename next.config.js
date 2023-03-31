de0f38f5db75060488766ae2793b057f008b3d9c
killroy192 <killroy192@gmail.com>
2023-03-31 16:10:17 +0200

    pass env to app

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
     hideSourceMaps: true,
     disableServerWebpackPlugin: !process.env.SENTRY_DSN,
     disableClientWebpackPlugin: !process.env.NEXT_PUBLIC_SENTRY_DSN,
+  },
+  env: {
+    networkName: process?.env?.NETWORK_NAME || 'rabbit.dev',
+    chainId: process?.env?.CHAIN_ID_HEX || '0x658d8',
+    rpc: process?.env?.RPC || 'https://dev.dgma.dev:8441',
   }
 }
 
